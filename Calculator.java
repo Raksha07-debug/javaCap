de95d57e2d6f2c1c0260f60feb4d949f4a2fd781
Raksha <rak070803@gmail.com>
2025-01-07 10:45:40 +0530

    initail commit

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,10 @@
 class Calculator{
  public static void main (String args[]){
-System.out.println("welcome to the  calculator );
+System.out.println("welcome to the  calculator" );
+System.out.println(addition(3,6));
+}
+public static int addition(int a,int b){
+return a+b;
 
 }
 }
