f6993414a86d71239947c1d7ef7a05e8643e5592
Raksha <rak070803@gmail.com>
2025-01-07 11:08:20 +0530

    sub function

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
-class Hello{
-    public static void main(String[] args) {
-        System.out.println("hello world");
-    }
+class Hello world{
+public static void main(String args[]){
+ System.out.println("Hello world");
+}
 }
